e0e6140f594bb22e3f20f5d096e9690e0fe436dd
Matheus Ferreira <matheus.play.baixista@hotmail.com>
2019-07-24 01:02:19 -0300

    Add files via upload

---
+++ projeto_01.sql
@@ -0,0 +1,255 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 24-Jul-2019 às 06:00
+-- Versão do servidor: 10.1.39-MariaDB
+-- versão do PHP: 7.1.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projeto_01`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.online`
+--
+
+CREATE TABLE `tb_admin.online` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `ultima_acao` datetime NOT NULL,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_admin.online`
+--
+
+INSERT INTO `tb_admin.online` (`id`, `ip`, `ultima_acao`, `token`) VALUES
+(2, '::1', '2019-07-24 00:44:36', '5d37bed10f2ff');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.usuarios`
+--
+
+CREATE TABLE `tb_admin.usuarios` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cargo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_admin.usuarios`
+--
+
+INSERT INTO `tb_admin.usuarios` (`id`, `user`, `password`, `img`, `nome`, `cargo`) VALUES
+(1, 'admin', 'admin', '', 'Admin', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.visitas`
+--
+
+CREATE TABLE `tb_admin.visitas` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `dia` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_site.config`
+--
+
+CREATE TABLE `tb_site.config` (
+  `titulo` varchar(255) NOT NULL,
+  `nome_autor` varchar(255) NOT NULL,
+  `descricao` text NOT NULL,
+  `icone1` varchar(255) NOT NULL,
+  `descricao1` text NOT NULL,
+  `icone2` varchar(255) NOT NULL,
+  `descricao2` text NOT NULL,
+  `icone3` varchar(255) NOT NULL,
+  `descricao3` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_site.config`
+--
+
+INSERT INTO `tb_site.config` (`titulo`, `nome_autor`, `descricao`, `icone1`, `descricao1`, `icone2`, `descricao2`, `icone3`, `descricao3`) VALUES
+('Projeto Site Dinâmico', 'Matheus Ferreira', 'Descrição do autor', 'fab fa-css3', 'Descrição do css3', 'fab fa-html5', 'Descrição do html5', 'fab fa-js-square', 'Descrição do js');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_site.depoimentos`
+--
+
+CREATE TABLE `tb_site.depoimentos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `depoimento` text NOT NULL,
+  `data` date NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_site.depoimentos`
+--
+
+INSERT INTO `tb_site.depoimentos` (`id`, `nome`, `depoimento`, `data`, `order_id`) VALUES
+(1, 'Fulano 1', 'Depoimento 1', '2019-07-23', 1),
+(2, 'Fulano 2', 'Depoimento 2', '2019-07-23', 2),
+(3, 'Fulano 2', 'Depoimento 2', '2019-07-23', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_site.servicos`
+--
+
+CREATE TABLE `tb_site.servicos` (
+  `id` int(11) NOT NULL,
+  `servico` text NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_site.servicos`
+--
+
+INSERT INTO `tb_site.servicos` (`id`, `servico`, `order_id`) VALUES
+(1, 'Serviço 1', 1),
+(2, 'Serviço 2', 2),
+(3, 'Serviço 3', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_sites.slides`
+--
+
+CREATE TABLE `tb_sites.slides` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `slide` varchar(255) NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_sites.slides`
+--
+
+INSERT INTO `tb_sites.slides` (`id`, `nome`, `slide`, `order_id`) VALUES
+(1, 'slide1', '5d37bebc2461c.jpg', 1),
+(2, 'slide2', '5d37bec10c08b.jpg', 2),
+(3, 'slide3', '5d37bec575909.jpg', 3),
+(4, 'slide4', '5d37beca45894.jpg', 4),
+(5, 'slide5', '5d37beceea657.jpg', 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin.online`
+--
+ALTER TABLE `tb_admin.online`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_admin.usuarios`
+--
+ALTER TABLE `tb_admin.usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_admin.visitas`
+--
+ALTER TABLE `tb_admin.visitas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_site.depoimentos`
+--
+ALTER TABLE `tb_site.depoimentos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_site.servicos`
+--
+ALTER TABLE `tb_site.servicos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_sites.slides`
+--
+ALTER TABLE `tb_sites.slides`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_admin.online`
+--
+ALTER TABLE `tb_admin.online`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_admin.usuarios`
+--
+ALTER TABLE `tb_admin.usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_admin.visitas`
+--
+ALTER TABLE `tb_admin.visitas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_site.depoimentos`
+--
+ALTER TABLE `tb_site.depoimentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tb_site.servicos`
+--
+ALTER TABLE `tb_site.servicos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_sites.slides`
+--
+ALTER TABLE `tb_sites.slides`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
